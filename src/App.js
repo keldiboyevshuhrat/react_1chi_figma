4cec7710f046549bb547a5579965304fed62c7b8
shuhrat <keldiboyevshuhrat062@gmail.com>
2024-06-20 14:31:48 +0500

    new fille push git hap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,318 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';      // rasim obkelish uchun 
+import rasim from "./Oval.png"
+
+import rasim2 from "./bulb 2.png"
+
+import './App.css';                    // fayillar chaqirish uchun 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+
+    <div className="react_1">
+
+
+
+      <div className="react_2">
+        <div className="react_2_img">
+          <img src={rasim} className="react_2_img1" />
+          <h2>Frontend Mentor</h2>
+          <p>Feedback Board</p>
+        </div>
+        <div className="react_2_div2">
+          <div className="react_2_div2_1">
+            <div className="react_2_div2_1_1">All</div>
+            <div className="react_2_div2_1_1">Ui</div>
+            <div className="react_2_div2_1_1">UX</div>
+
+          </div>
+          <div className="react_2_div2_1">
+            <div className="react_2_div2_1_2">Enhancement</div>
+            <div className="react_2_div2_1_3">Bug</div>
+
+          </div>
+          <div className="react_2_div2_1">
+            <div className="react_2_div2_1_4">Feature</div>
+
+          </div>
+
+        </div>
+
+        <div className="react_2_div3">
+          <div className="react_2_div3_1">
+            <p>Roadmap</p> <a href="#">View</a>
+          </div>
+
+          <div className="react_2_div3_1">
+            <div className="react_2_div3_1_rang">
+              <div className="react_2_div3_1_rang_1"></div>
+
+              <p>Planned</p>
+            </div>
+            <h5>3</h5>
+          </div>
+          <div className="react_2_div3_1">
+            <div className="react_2_div3_1_rang">
+              <div className="react_2_div3_1_rang_2"></div>
+
+              <p>In-Progress</p>
+            </div>
+            <h5>2</h5>
+          </div>
+          <div className="react_2_div3_1">
+            <div className="react_2_div3_1_rang">
+              <div className="react_2_div3_1_rang_3"></div>
+              <p>Live</p>
+            </div>
+            <h5>1</h5>
+
+          </div>
+
+
+
+        </div>
+
+
+
+      </div>
+
+      {/* div 2  */}
+
+
+
+      <div className="react_3">
+
+        <div className="react_3_div1">
+
+          <div className="react_3_div1_img">
+            <img src={rasim2} />
+            <h4>6 Suggestions</h4>
+            <p>sort by :</p>
+
+            <select name="" id="">
+              <option value=""> Most Upvotes</option>
+
+
+            </select>
+
+          </div>
+          <button>+ add feedback </button>
+
+        </div>
+
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>112</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Add tags for solutions</h1>
+              <p>Easier to search for solutions based on a specific stack.</p>
+              <div className="react_3_div2_div_2_div">Enhancement</div>
+            </div>
+
+          </div>
+          {/* 2 */}
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">2</p>
+          </div>
+
+
+        </div>
+
+
+
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>99</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Add a dark theme option</h1>
+              <p>It would help people with light sensitivities and who prefer dark mode.</p>
+              <div className="react_3_div2_div_2_div">Feature</div>
+            </div>
+
+          </div>
+
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">4</p>
+          </div>
+
+
+        </div>
+        {/* 3 */}
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>65</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Q&A within the challenge hubs</h1>
+              <p>Challenge-specific Q&A would make for easy reference.</p>
+              <div className="react_3_div2_div_2_div">Feature</div>
+            </div>
+
+          </div>
+
+
+
+
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">1</p>
+          </div>
+
+
+        </div>
+
+
+
+
+        {/* 4 */}
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>51</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Allow image/video upload to feedback</h1>
+              <p>Images and screencasts can enhance comments on solutions.</p>
+              <div className="react_3_div2_div_2_div">Enhancement</div>
+            </div>
+
+          </div>
+
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">2</p>
+          </div>
+
+
+        </div>
+
+
+
+
+
+        {/* 5 */}
+
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>42</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Ability to follow others</h1>
+              <p>Stay updated on comments and solutions other people post.</p>
+              <div className="react_3_div2_div_2_div">Feature</div>
+            </div>
+
+          </div>
+
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">3</p>
+          </div>
+
+
+        </div>
+
+        {/* 6 */}
+
+
+        <div className="react_3_div2">
+
+          <div className="react_3_div2_div">
+            <div className="react_3_div2_1">
+              <i class="react_3_div2_1_icons fa-solid fa-angle-up"></i>
+              <p>4</p>
+            </div>
+            <div className="react_3_div2_div_2">
+              <h1>Preview images not loading</h1>
+              <p>Challenge preview images are missing when you apply a filter.</p>
+              <div className="react_3_div2_div_2_div">bug</div>
+            </div>
+
+          </div>
+
+          <div className="react_3_div2_div2">
+            <i class="react_3_div2_div2_icons fa-solid fa-comment"></i>
+            <p id="p_id">0</p>
+          </div>
+
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+
+
+
     </div>
+
+
+
+    //  <i class="fa-solid fa-comment"></i> 
+
+
+
+
+
+
+
+
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+
+
+
+
+
   );
 }
 
 export default App;
+
+
---
+++ src/json.js
@@ -0,0 +1,54 @@
+let json1 = [
+
+    {
+        "id": 1,
+        "son": 112,
+        "nom1": "Add tags for solutions",
+        "nom2": "Easier to search for solutions based on a specific stack.",
+        "tugma": "Enhancement",
+        "comment": 2,
+    },
+    {
+        "id": 2,
+        "son": 99,
+        "nom1": "Add a dark theme option",
+        "nom2": "It would help people with light sensitivities and who prefer dark mode.",
+        "tugma": "Feature",
+        "comment": 4,
+    },
+    {
+        "id": 3,
+        "son": 65,
+        "nom1": "Q&A within the challenge hubs",
+        "nom2": "Challenge-specific Q&A would make for easy reference",
+        "tugma": "Feature",
+        "comment": 1,
+    },
+    {
+        "id": 4,
+        "son": 51,
+        "nom1": "Allow image/video upload to feedback",
+        "nom2": "Images and screencasts can enhance comments on solutions.",
+        "tugma": "Enhancement",
+        "comment": 2,
+    },
+    {
+        "id": 5,
+        "son": 42,
+        "nom1": "Ability to follow others",
+        "nom2": "Stay updated on comments and solutions other people post.",
+        "tugma": "Feature",
+        "comment": 3,
+    },
+    {
+        "id": 6,
+        "son": 4,
+        "nom1": "Preview images not loading",
+        "nom2": "Challenge preview images are missing when you apply a filter.",
+        "tugma": "bug",
+        "comment": 0,
+    },
+ 
+
+]
+console.log(json1);
